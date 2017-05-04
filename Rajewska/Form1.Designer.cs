322f1bd2adddcadd7a7325f187f882f76c5f99a2
Robson10 <mucharobert23@gmail.com>
2017-05-04 02:18:41 +0200

    Add project files.

---
+++ Rajewska/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Rajewska
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Jawny = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Cezar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.Polibiusz = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Gaderypoluki = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Vigener = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.At = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Bacon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Rot = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.Przestawieniowy = new System.Windows.Forms.TextBox();
+            this.L = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.tabPage8.SuspendLayout();
+            this.tabPage9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tekst jawny";
+            // 
+            // Jawny
+            // 
+            this.Jawny.Location = new System.Drawing.Point(82, 12);
+            this.Jawny.Multiline = true;
+            this.Jawny.Name = "Jawny";
+            this.Jawny.Size = new System.Drawing.Size(185, 68);
+            this.Jawny.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Szyfruj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Controls.Add(this.tabPage8);
+            this.tabControl1.Controls.Add(this.tabPage9);
+            this.tabControl1.Location = new System.Drawing.Point(12, 86);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(644, 337);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Cezar);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(636, 311);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Cezar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.Polibiusz);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(636, 311);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Polibiusz";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Cezar
+            // 
+            this.Cezar.Location = new System.Drawing.Point(6, 26);
+            this.Cezar.Multiline = true;
+            this.Cezar.Name = "Cezar";
+            this.Cezar.Size = new System.Drawing.Size(621, 66);
+            this.Cezar.TabIndex = 53;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Cezar";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.Gaderypoluki);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(636, 311);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Gaderypoluki";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.Vigener);
+            this.tabPage4.Controls.Add(this.label6);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(636, 311);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Vigener";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.At);
+            this.tabPage6.Controls.Add(this.label7);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(636, 311);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "AtBash";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.Bacon);
+            this.tabPage7.Controls.Add(this.label8);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(636, 311);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Bacon";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Controls.Add(this.Rot);
+            this.tabPage8.Controls.Add(this.label9);
+            this.tabPage8.Location = new System.Drawing.Point(4, 22);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage8.Size = new System.Drawing.Size(636, 311);
+            this.tabPage8.TabIndex = 7;
+            this.tabPage8.Text = "ROT13";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // Polibiusz
+            // 
+            this.Polibiusz.Location = new System.Drawing.Point(9, 26);
+            this.Polibiusz.Multiline = true;
+            this.Polibiusz.Name = "Polibiusz";
+            this.Polibiusz.Size = new System.Drawing.Size(621, 20);
+            this.Polibiusz.TabIndex = 57;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Polibiusz";
+            // 
+            // Gaderypoluki
+            // 
+            this.Gaderypoluki.Location = new System.Drawing.Point(9, 26);
+            this.Gaderypoluki.Multiline = true;
+            this.Gaderypoluki.Name = "Gaderypoluki";
+            this.Gaderypoluki.Size = new System.Drawing.Size(621, 20);
+            this.Gaderypoluki.TabIndex = 59;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Gaderypoluki";
+            // 
+            // Vigener
+            // 
+            this.Vigener.Location = new System.Drawing.Point(3, 26);
+            this.Vigener.Multiline = true;
+            this.Vigener.Name = "Vigener";
+            this.Vigener.Size = new System.Drawing.Size(627, 20);
+            this.Vigener.TabIndex = 61;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "Vigenera";
+            // 
+            // At
+            // 
+            this.At.Location = new System.Drawing.Point(6, 26);
+            this.At.Multiline = true;
+            this.At.Name = "At";
+            this.At.Size = new System.Drawing.Size(624, 20);
+            this.At.TabIndex = 63;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 62;
+            this.label7.Text = "AtBash";
+            // 
+            // Bacon
+            // 
+            this.Bacon.Location = new System.Drawing.Point(6, 26);
+            this.Bacon.Multiline = true;
+            this.Bacon.Name = "Bacon";
+            this.Bacon.Size = new System.Drawing.Size(624, 20);
+            this.Bacon.TabIndex = 65;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 64;
+            this.label8.Text = "Bacon";
+            // 
+            // Rot
+            // 
+            this.Rot.Location = new System.Drawing.Point(6, 26);
+            this.Rot.Multiline = true;
+            this.Rot.Name = "Rot";
+            this.Rot.Size = new System.Drawing.Size(624, 20);
+            this.Rot.TabIndex = 67;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 66;
+            this.label9.Text = "Rot13";
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Controls.Add(this.Przestawieniowy);
+            this.tabPage9.Controls.Add(this.L);
+            this.tabPage9.Location = new System.Drawing.Point(4, 22);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage9.Size = new System.Drawing.Size(636, 311);
+            this.tabPage9.TabIndex = 8;
+            this.tabPage9.Text = "Przestawieniowy";
+            this.tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // Przestawieniowy
+            // 
+            this.Przestawieniowy.Location = new System.Drawing.Point(9, 26);
+            this.Przestawieniowy.Multiline = true;
+            this.Przestawieniowy.Name = "Przestawieniowy";
+            this.Przestawieniowy.Size = new System.Drawing.Size(621, 20);
+            this.Przestawieniowy.TabIndex = 69;
+            // 
+            // L
+            // 
+            this.L.AutoSize = true;
+            this.L.Location = new System.Drawing.Point(6, 10);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(85, 13);
+            this.L.TabIndex = 68;
+            this.L.Text = "Przestawieniowy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 435);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Jawny);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage7.PerformLayout();
+            this.tabPage8.ResumeLayout(false);
+            this.tabPage8.PerformLayout();
+            this.tabPage9.ResumeLayout(false);
+            this.tabPage9.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Jawny;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox Cezar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox Polibiusz;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox Gaderypoluki;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox Vigener;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TextBox At;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TextBox Bacon;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TabPage tabPage8;
+        private System.Windows.Forms.TextBox Rot;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TabPage tabPage9;
+        private System.Windows.Forms.TextBox Przestawieniowy;
+        private System.Windows.Forms.Label L;
+    }
+}
+
---
+++ Rajewska/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rajewska
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //String alfabetMale = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż";//35 znaków
+        String alfabetMale = "abcdefghijklmnopqrstuvwxyz";//35 znaków
+        String alfabetDuze;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            alfabetDuze = alfabetMale.ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cezar.Text= SzyfrCezar(Jawny.Text,-1);
+            Rot.Text= SzyfrRot13(Jawny.Text);
+            Polibiusz.Text = SzyfrPolibiusza(Jawny.Text);
+            Gaderypoluki.Text = SzyfrGaderypoluki(Jawny.Text, "GA - DE - RY - PO - LU - KI");
+            Przestawieniowy.Text = SzyfrPrzestawieniowy(Jawny.Text,3);
+            At.Text = AtBash(Jawny.Text);
+            Bacon.Text = SzyfrBacona(Jawny.Text);
+            Vigener.Text= SzyfrVigener(Jawny.Text, "TAJNE");
+        }
+        private string SzyfrCezar(string value, int shift)
+        {
+            char[] buffer = value.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                if (letter != ' ')
+                {
+                    int index = -1;
+                    index = alfabetMale.IndexOf(letter);
+                    if (index!=-1)
+                    {
+                        index+=shift;
+                        if (index > 0) index = (index >= alfabetMale.Count()) ? (alfabetMale.Length - 1 + shift) - alfabetMale.Length : index;
+                        else index = (alfabetMale.Length - 1) - alfabetMale.IndexOf(letter);
+                        letter = alfabetMale[index];
+                    }
+
+                    index = alfabetDuze.IndexOf(letter);
+                    if (index != -1)
+                    {
+                        index+=shift;
+                        if (index>0) index = (index >= alfabetDuze.Count()) ? (alfabetDuze.Length - 1 + shift) - alfabetDuze.Length : index;
+                        else index = (alfabetDuze.Length - 1) - alfabetDuze.IndexOf(letter);
+                        letter = alfabetDuze[index];
+                    }
+                    buffer[i] = letter;
+                }
+            }
+            return new string(buffer);
+        }
+        private string SzyfrRot13(string value)
+        {
+            char[] buffer = value.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                if (letter != ' ')
+                {
+                    int index = -1;
+                    index = alfabetMale.IndexOf(letter);
+                    if (index != -1)
+                    {
+                        index += 13;
+                        index = (index >= alfabetMale.Count()) ? (alfabetMale.Length - 1 + 13) - alfabetMale.Length : index;
+                        letter = alfabetMale[index];
+                    }
+
+                    index = alfabetDuze.IndexOf(letter);
+                    if (index != -1)
+                    {
+                        index += 13;
+                        index = (index >= alfabetDuze.Count()) ? (alfabetDuze.Length - 1 + 13) - alfabetDuze.Length : index;
+                        letter = alfabetDuze[index];
+                    }
+                    buffer[i] = letter;
+                }
+            }
+            return new string(buffer);
+        }
+        private string SzyfrPolibiusza(string text)
+        {
+            text=text.ToLower();
+            text = text.Replace('j', 'i');
+            string result = "";
+            char[,] szachownica = new char[,]
+            {
+                {'a','b','c','d','e' },
+                {'f','g','h','i','k' },
+                {'l','m','n','o','p' },
+                {'q','r','s','t','u' },
+                {'v','w','x','y','z' }
+            };
+
+            for (int i = 0; i < text.Length; i++)
+                if (text[i]== ' ')
+                    result += " ";
+                else
+                    for (int j = 0; j < 5; j++)
+                        for (int k = 0; k < 5; k++)
+                            if (text[i] == szachownica[j, k])
+                                result += j.ToString() + k.ToString()+" ";
+            return result;
+        }
+        private string SzyfrGaderypoluki(string text, string haslo)
+        {
+            StringBuilder bufor = new StringBuilder(text.ToUpper());
+            haslo = haslo.ToUpper();
+            haslo=haslo.Replace("-", "");
+            haslo = haslo.Replace(" ", "");
+
+            for (int i = 0; i < bufor.Length; i ++)
+            {
+                for (int i2 = 0; i2 < haslo.Length; i2+=2)
+                {
+                    char a = haslo[i2];
+                    char b = haslo[i2 + 1];
+                    if (bufor[i] == a)
+                    {
+                        bufor[i] = b;
+                        break;
+                    }
+                    if (bufor[i] == b)
+                    {
+                        bufor[i] = a;
+                        break;
+                    }
+                }
+                
+            }
+            return bufor.ToString();
+        }
+        private string SzyfrPrzestawieniowy(string text, int wGrupie)
+        {
+            StringBuilder temp = new StringBuilder(text);
+            for (int i = 0; i < temp.Length; i+=wGrupie)
+                for (int i2 = i; i2 <(i+wGrupie/2) && (i2+wGrupie<=temp.Length); i2++)
+                {
+                    int id = i + wGrupie-1 ;
+                    char x = temp[id];
+                    temp[id] = temp[i2];
+                    temp[i2] = x;
+                }
+            return temp.ToString();
+        }
+        private string SzyfrBacona(string text)
+        {
+            string alfabet = alfabetMale.Replace("j", "").Replace("v", "");
+            text = text.ToLower();
+            StringBuilder temp = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+                if(text[i]!=' ')
+                    temp.Append(Convert.ToString(alfabet.IndexOf(text[i]), 2).PadLeft(5, '0').Replace("0", "A").Replace("1", "B") + " ");
+            return temp.ToString();
+        }
+        private string AtBash(string text)
+        {
+                text=text.ToLower();
+                char[] tablica = text.ToCharArray();
+                for (int i = 0; i < text.Length; i++)
+                    if (!text[i].Equals(" "))
+                        if (alfabetMale.IndexOf(text[i])!=-1)
+                            tablica[i] = alfabetMale[ alfabetMale.Length-1-alfabetMale.IndexOf(text[i])];
+                        else if (alfabetDuze.IndexOf(text[i]) != -1)
+                            tablica[i] = alfabetDuze[alfabetDuze.Length - 1 - alfabetDuze.IndexOf(text[i])];
+            return new string(tablica);
+        }
+        private string SzyfrVigener(string text,string haslo)
+        {
+            haslo = haslo.ToUpper();
+            text = text.ToUpper();
+            List<string> tablica = new List<string>();
+            for (int i = 0; i < alfabetDuze.Length; i++)
+            {
+                string alfabet = "";
+                for (int i2 = 0; i2 < alfabetDuze.Length; i2++)
+                {
+                    int index = i2 + i;
+                    index = (index >= alfabetDuze.Length) ? index - alfabetDuze.Length : index;
+                    alfabet += alfabetDuze[index];
+                }
+                tablica.Add(alfabet);
+            }
+            int x=0;
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    char literaHasla = haslo[x % haslo.Length];
+                    char literaTekstu = text[i];
+                    int indexTekstu = alfabetDuze.IndexOf(literaTekstu);
+                    int indexHasla = tablica[0].IndexOf(literaHasla);
+                    temp.Append(tablica[indexTekstu][indexHasla]);
+                    x++;
+                }
+                else temp.Append(" ");
+            }
+            return temp.ToString();
+        }
+    }
+}
